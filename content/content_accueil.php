d1dce6d18156cb07b34b5823d89dee096a220483
DGothrek <35027141+DGothrek@users.noreply.github.com>
2018-04-04 13:26:36 +0200

    Concerts

--- content/content_accueil.php
+++ content/content_accueil.php
@@ -1,13 +1,27 @@
 
-
-
-<div class="row">
-    <div class="col-md-offset-2 col-md-4 col-md-offset-2">
-        <h3>Des programmes ambitieux...</h3>
-
-    </div>
-    <div class="col-md-4 col-md-offset-1">
-        <h3>... pour des concerts exceptionnels</h3>
+<div class="row description">
+    <div class="col-xs-offset-1 col-sm-offset-2 col-xs-10 col-md-8">
+        <h3>Des programmes ambitieux</h3>
+        <p>
+            Travailler avec l'ensemble vocal de l'X, c'est un vrai plaisir pour tout étudiant mélomane !
+            Montius nos tumore inusitato quodam et novo ut rebellis et maiestati recalcitrantes Augustae per haec quae strepit incusat iratus nimirum quod contumacem praefectum, quid rerum ordo postulat ignorare dissimulantem formidine tenus iusserim custodiri.
+        </p>
 
     </div>
+
+</div>
+
+<div class='row video-player'>
+    <div class='col-xs-offset-1 col-sm-offset-2 col-xs-10 col-md-8 concert-video-title'>
+        <h3>Carmina Burana à l'École polytechnique</h3>
+    </div>
+    <div class = "col-xs-offset-1 col-sm-offset-2 col-xs-10 col-md-8" id="ytplayer">
+        <iframe id="ytplayer" type="text/html" width="100%" height="405"
+                src="https://www.youtube.com/embed/dUgC8G_yoV4?origin=http://localhost&modestbranding=1"
+                frameborder="0" allowfullscreen>
+        </iframe>
+    </div>
+
+
+
 </div>
